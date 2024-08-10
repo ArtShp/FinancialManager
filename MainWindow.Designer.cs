862789247de64881193f84fd90c7ce3f26ecd819
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-08-10 16:55:04 +0200

    Change MainWindow name

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FinancialManager
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "MainWindow";
+            Text = "Financial Manager";
+            ResumeLayout(false);
         }
 
         #endregion
