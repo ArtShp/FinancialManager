051c04a2223bbab88d95d2a7a339edfc972c47f9
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-08-21 13:48:47 +0200

    Edit places of components and labels names

--- EditTransactionsForm.Designer.cs
+++ EditTransactionsForm.Designer.cs
@@ -28,18 +28,275 @@
         /// </summary>
         private void InitializeComponent()
         {
+            titleLabel = new Label();
+            transactionTypeLabel = new Label();
+            comboBox1 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateLabel = new Label();
+            sumLabel = new Label();
+            textBox1 = new TextBox();
+            comboBox2 = new ComboBox();
+            cashFacilityLabel = new Label();
+            comboBox3 = new ComboBox();
+            placeLabel = new Label();
+            comboBox4 = new ComboBox();
+            currencyLabel = new Label();
+            descriptionLabel = new Label();
+            richTextBox1 = new RichTextBox();
+            listView1 = new ListView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
             SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            titleLabel.Location = new Point(41, 12);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new Size(308, 65);
+            titleLabel.TabIndex = 0;
+            titleLabel.Text = "Transactions";
+            // 
+            // transactionTypeLabel
+            // 
+            transactionTypeLabel.AutoSize = true;
+            transactionTypeLabel.Location = new Point(41, 112);
+            transactionTypeLabel.Name = "transactionTypeLabel";
+            transactionTypeLabel.Size = new Size(192, 32);
+            transactionTypeLabel.TabIndex = 1;
+            transactionTypeLabel.Text = "Transaction Type";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(41, 147);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(379, 40);
+            comboBox1.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(41, 270);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(379, 39);
+            dateTimePicker1.TabIndex = 3;
+            // 
+            // dateLabel
+            // 
+            dateLabel.AutoSize = true;
+            dateLabel.Location = new Point(41, 235);
+            dateLabel.Name = "dateLabel";
+            dateLabel.Size = new Size(64, 32);
+            dateLabel.TabIndex = 4;
+            dateLabel.Text = "Date";
+            // 
+            // sumLabel
+            // 
+            sumLabel.AutoSize = true;
+            sumLabel.Location = new Point(41, 369);
+            sumLabel.Name = "sumLabel";
+            sumLabel.Size = new Size(62, 32);
+            sumLabel.TabIndex = 5;
+            sumLabel.Text = "Sum";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(41, 404);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(379, 39);
+            textBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(483, 147);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(377, 40);
+            comboBox2.TabIndex = 8;
+            // 
+            // cashFacilityLabel
+            // 
+            cashFacilityLabel.AutoSize = true;
+            cashFacilityLabel.Location = new Point(483, 112);
+            cashFacilityLabel.Name = "cashFacilityLabel";
+            cashFacilityLabel.Size = new Size(144, 32);
+            cashFacilityLabel.TabIndex = 7;
+            cashFacilityLabel.Text = "Cash Facility";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(483, 269);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(377, 40);
+            comboBox3.TabIndex = 10;
+            // 
+            // placeLabel
+            // 
+            placeLabel.AutoSize = true;
+            placeLabel.Location = new Point(483, 235);
+            placeLabel.Name = "placeLabel";
+            placeLabel.Size = new Size(69, 32);
+            placeLabel.TabIndex = 9;
+            placeLabel.Text = "Place";
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(483, 404);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(377, 40);
+            comboBox4.TabIndex = 12;
+            // 
+            // currencyLabel
+            // 
+            currencyLabel.AutoSize = true;
+            currencyLabel.Location = new Point(483, 369);
+            currencyLabel.Name = "currencyLabel";
+            currencyLabel.Size = new Size(109, 32);
+            currencyLabel.TabIndex = 11;
+            currencyLabel.Text = "Currency";
+            // 
+            // descriptionLabel
+            // 
+            descriptionLabel.AutoSize = true;
+            descriptionLabel.Location = new Point(41, 486);
+            descriptionLabel.Name = "descriptionLabel";
+            descriptionLabel.Size = new Size(135, 32);
+            descriptionLabel.TabIndex = 13;
+            descriptionLabel.Text = "Description";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(41, 521);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(379, 255);
+            richTextBox1.TabIndex = 14;
+            richTextBox1.Text = "";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(901, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(717, 980);
+            listView1.TabIndex = 15;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(41, 946);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 16;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(41, 894);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 17;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(236, 894);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 46);
+            button3.TabIndex = 19;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(236, 946);
+            button4.Name = "button4";
+            button4.Size = new Size(150, 46);
+            button4.TabIndex = 18;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(426, 894);
+            button5.Name = "button5";
+            button5.Size = new Size(150, 46);
+            button5.TabIndex = 21;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(426, 946);
+            button6.Name = "button6";
+            button6.Size = new Size(150, 46);
+            button6.TabIndex = 20;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
             // 
             // EditTransactionsForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1258, 1023);
+            ClientSize = new Size(1630, 1023);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listView1);
+            Controls.Add(richTextBox1);
+            Controls.Add(descriptionLabel);
+            Controls.Add(comboBox4);
+            Controls.Add(currencyLabel);
+            Controls.Add(comboBox3);
+            Controls.Add(placeLabel);
+            Controls.Add(comboBox2);
+            Controls.Add(cashFacilityLabel);
+            Controls.Add(textBox1);
+            Controls.Add(sumLabel);
+            Controls.Add(dateLabel);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox1);
+            Controls.Add(transactionTypeLabel);
+            Controls.Add(titleLabel);
             Name = "EditTransactionsForm";
             Text = "EditTransactionsForm";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label titleLabel;
+        private Label transactionTypeLabel;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private Label dateLabel;
+        private Label sumLabel;
+        private TextBox textBox1;
+        private ComboBox comboBox2;
+        private Label cashFacilityLabel;
+        private ComboBox comboBox3;
+        private Label placeLabel;
+        private ComboBox comboBox4;
+        private Label currencyLabel;
+        private Label descriptionLabel;
+        private RichTextBox richTextBox1;
+        private ListView listView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
