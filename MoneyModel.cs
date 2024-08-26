a325770154815243d275b8dc8a64e4dd4e97cae3
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-08-26 15:13:31 +0200

    Add money model

---
+++ MoneyModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManager
+{
+    internal class MoneyModel
+    {
+        public long Money { get; set; }
+        public int Rate { get; set; }
+
+        public string GetString()
+        {
+            long multiplier = Convert.ToInt64(Math.Pow(10, Rate));
+
+            long integer = Money / multiplier;
+            long fraction = Money % multiplier;
+
+            string strInteger = integer.ToString();
+            string strFraction = fraction.ToString();
+            if (strFraction.Length < Rate)
+            {
+                strFraction = new string('0', Rate - strFraction.Length) + strFraction;
+            }
+
+            return strInteger + "." + strFraction;
+        }
+
+        public long GetLong()
+        {
+            return Money;
+        }
+
+        public void SetValue(string number)
+        {
+            long multiplier = Convert.ToInt64(Math.Pow(10, Rate));
+
+            string[] parts = number.Split('.');
+
+            if (parts.Length == 1)
+            {
+                Money = Convert.ToInt64(parts[0]) * multiplier;
+            }
+            else
+            {
+                Money = Convert.ToInt64(parts[0]) * multiplier + Convert.ToInt64(parts[1]);
+            }
+        }
+
+        public void SetValue(long number)
+        {
+            Money = number;
+        }
+    }
+}
