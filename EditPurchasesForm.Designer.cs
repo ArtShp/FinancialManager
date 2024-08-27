dc31e982a86b9df33f2937a32e8b5d97d06dba18
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-08-27 11:33:58 +0200

    Add controls for purchase editing

--- EditPurchasesForm.Designer.cs
+++ EditPurchasesForm.Designer.cs
@@ -28,18 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            listView1 = new ListView();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 32);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(643, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(615, 908);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 134);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 32);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(31, 169);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(446, 40);
+            comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(31, 313);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(446, 40);
+            comboBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 278);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 32);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(31, 416);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 32);
+            label4.TabIndex = 6;
+            label4.Text = "label4";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(31, 451);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(446, 228);
+            richTextBox1.TabIndex = 7;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(31, 874);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 8;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(31, 822);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 9;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(245, 874);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 46);
+            button3.TabIndex = 10;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(245, 822);
+            button4.Name = "button4";
+            button4.Size = new Size(150, 46);
+            button4.TabIndex = 11;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(461, 874);
+            button5.Name = "button5";
+            button5.Size = new Size(150, 46);
+            button5.TabIndex = 12;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(461, 822);
+            button6.Name = "button6";
+            button6.Size = new Size(150, 46);
+            button6.TabIndex = 13;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
             // 
             // EditPurchasesForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1270, 950);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Controls.Add(label4);
+            Controls.Add(comboBox2);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(listView1);
+            Controls.Add(label1);
             Name = "EditPurchasesForm";
             Text = "EditPurchasesForm";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ListView listView1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label3;
+        private Label label4;
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
