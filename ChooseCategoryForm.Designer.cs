2114bed1b0b73c8bb9583e32d14815a2927c6835
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-08-27 18:12:19 +0200

    Add controls to choose category

--- ChooseCategoryForm.Designer.cs
+++ ChooseCategoryForm.Designer.cs
@@ -28,18 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            treeView1 = new TreeView();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 32);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new Point(426, 12);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(578, 711);
+            treeView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(27, 677);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(218, 677);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
             // 
             // ChooseCategoryForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1018, 834);
+            ClientSize = new Size(1016, 755);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(treeView1);
+            Controls.Add(label1);
             Name = "ChooseCategoryForm";
             Text = "ChooseCategoryForm";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TreeView treeView1;
+        private Button button1;
+        private Button button2;
     }
 }
