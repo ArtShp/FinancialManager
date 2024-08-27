d713b4f9ae382b0ef5dbe26bdaa8f09bd00808b2
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-08-27 22:45:18 +0200

    Add funcs to load categories for choosing

--- ChooseCategoryForm.cs
+++ ChooseCategoryForm.cs
@@ -12,9 +12,55 @@ namespace FinancialManager
 {
     public partial class ChooseCategoryForm : Form
     {
+        private List<CategoryModel> data = new List<CategoryModel>();
+        private long selectedId = -1;
+
         public ChooseCategoryForm()
         {
             InitializeComponent();
+
+            LoadMainCategories();
+            LoadMainSubCategories();
+        }
+
+        private void LoadMainCategories()
+        {
+            data = SqliteDataAccess.LoadCategoriesByParentId();
+
+            treeView.BeginUpdate();
+            treeView.Nodes.Clear();
+
+            foreach (var category in data)
+            {
+                treeView.Nodes.Add(
+                    new TreeNode(category.Name)
+                    {
+                        Tag = category.Id
+                    });
+            }
+            treeView.EndUpdate();
+        }
+
+        private void LoadMainSubCategories()
+        {
+            treeView.BeginUpdate();
+
+            foreach (var parent in treeView.Nodes)
+            {
+                var parentNode = (TreeNode)parent;
+                var categories = SqliteDataAccess.LoadCategoriesByParentId(Convert.ToInt64(parentNode.Tag));
+
+                foreach (var category in categories)
+                {
+                    parentNode.Nodes.Add(
+                        new TreeNode(category.Name)
+                        {
+                            Tag = category.Id
+                        });
+                }
+            }
+
+            treeView.EndUpdate();
         }
     }
 }
