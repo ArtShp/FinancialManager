90adeaf76a034a315b26755b4e9c494b74c473a4
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-08-27 13:33:36 +0200

    Add funcs to load purchases

--- EditPurchasesForm.cs
+++ EditPurchasesForm.cs
@@ -12,9 +12,41 @@ namespace FinancialManager
 {
     public partial class EditPurchasesForm : Form
     {
-        public EditPurchasesForm()
+        private List<PurchaseModel> data = new List<PurchaseModel>();
+        private long selectedId = -1;
+        private long transactionId;
+
+        public EditPurchasesForm(long transactionId)
         {
+            this.transactionId = transactionId;
+
             InitializeComponent();
+        }
+
+        private void LoadList()
+        {
+            data = SqliteDataAccess.LoadPurchases(transactionId);
+
+            UpdateList();
+        }
+
+        private void UpdateList()
+        {
+            listView.BeginUpdate();
+            listView.Items.Clear();
+
+            foreach (var purchase in data)
+            {
+                var category = SqliteDataAccess.GetCategoryById(purchase.Id_Category);
+                listView.Items.Add(
+                    new ListViewItem(new[] { category.Name, purchase.Sum_By_Transaction.GetString(), purchase.Description })
+                    {
+                        Tag = purchase.Id
+                    });
+            }
+
+            listView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            listView.EndUpdate();
         }
     }
 }
