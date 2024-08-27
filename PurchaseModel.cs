b919054860fe41aebec6fb71046538f2a42c0ea6
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-08-27 11:17:36 +0200

    Add purchase model

---
+++ PurchaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManager
+{
+    internal class PurchaseModel
+    {
+        public long Id { get; set; }
+        public long Id_Transaction { get; set; }
+        public long Sum_By_Transaction { get; set; }
+        public long Id_Category { get; set; }
+        public string Description { get; set; }
+    }
+}
