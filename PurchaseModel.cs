fbfa247dd0876d47c13fa4cbf468bf2a3de43778
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-02 17:55:46 +0200

    Change Purchase model
    Change for changed DB structure

--- PurchaseModel.cs
+++ PurchaseModel.cs
@@ -10,7 +10,8 @@ namespace FinancialManager
     {
         public long Id { get; set; }
         public long Id_Transaction { get; set; }
-        public MoneyModel Sum_By_Transaction { get; set; }
+        public MoneyModel Sum { get; set; }
+        public MoneyModel Sum_By_Main_Currency { get; set; }
         public long Id_Category { get; set; }
         public string Description { get; set; }
     }
