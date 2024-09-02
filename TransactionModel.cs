4cb78e8c967a21e392295de722f42e21bc044237
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-02 18:04:43 +0200

    Change transaction model
    Change for changed DB structure

--- TransactionModel.cs
+++ TransactionModel.cs
@@ -12,7 +12,7 @@ namespace FinancialManager
         public long Id { get; set; }
         public long Id_Transaction_Type { get; set; }
         public DateTime Date { get; set; }
-        public MoneyModel Sum_By_Account { get; set; }
+        public MoneyModel Sum_By_Cash_Facility { get; set; }
         public long Id_Currency_Of_Transaction { get; set; }
         public long Id_Cash_Facility { get; set; }
         public long Id_Place_Of_Purchase { get; set; }
