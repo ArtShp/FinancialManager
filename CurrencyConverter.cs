1d5a1890d9c62b5ab89c546b1ca3c66fb5354dd0
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-03 19:46:42 +0200

    Add class to convert money (by currencies)

--- CurrencyConverter.cs
+++ CurrencyConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace FinancialManager
 {
     internal static class CurrencyConverter
     {
+        public static MoneyModel ConvertMoney(MoneyModel money, CurrencyModel fromCurrency, CurrencyModel toCurrency)
+        {
+            if (fromCurrency.Code == toCurrency.Code)
+            {
+                return money;
+            }
+
+            money.Rate = fromCurrency.Units_Rate;
+
+            Decimal sum = Convert.ToDecimal(money.GetString()) * GetCurrencyRate(fromCurrency, toCurrency);
+
+            return new MoneyModel(sum.ToString(), toCurrency.Units_Rate);
+        }
+
+        private static Decimal GetCurrencyRate(CurrencyModel fromCurrency, CurrencyModel toCurrency)
+        {
+            return CurrencyAPI.GetCurrencyRate(fromCurrency, toCurrency);
+        }
     }
 }
