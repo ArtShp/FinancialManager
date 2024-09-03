d316e9ee57a34c1d9291ccda0e5019670482d3f8
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-03 19:45:49 +0200

    Add class to work with currency API
    Works with https://currencyapi.com/ : API, to get currency exchange rates

---
+++ CurrencyAPI.cs
@@ -0,0 +1,48 @@
+﻿using currencyapi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FinancialManager
+{
+    internal static class CurrencyAPI
+    {
+        public static Decimal GetCurrencyRate(CurrencyModel fromCurrency, CurrencyModel toCurrency)
+        {
+            var fx = new Currencyapi(Properties.Resources.CurrencyApiKey);
+            var request = fx.Latest(fromCurrency.Code, toCurrency.Code);
+
+            CurrencyData currencyData = JsonSerializer.Deserialize<CurrencyData>(request);
+
+            return currencyData.Data[toCurrency.Code].Value;
+        }
+
+        private class CurrencyData
+        {
+            [JsonPropertyName("meta")]
+            public Meta Meta { get; set; }
+
+            [JsonPropertyName("data")]
+            public Dictionary<string, CurrencyInfo> Data { get; set; }
+        }
+
+        private class Meta
+        {
+            [JsonPropertyName("last_updated_at")]
+            public DateTime LastUpdatedAt { get; set; }
+        }
+
+        private class CurrencyInfo
+        {
+            [JsonPropertyName("code")]
+            public string Code { get; set; }
+
+            [JsonPropertyName("value")]
+            public Decimal Value { get; set; }
+        }
+    }
+}
