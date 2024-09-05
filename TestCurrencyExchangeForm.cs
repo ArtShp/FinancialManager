dfe941fd3b552ed90bb274c558f53be8d02f497b
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-05 12:15:46 +0200

    Add currency loading

--- TestCurrencyExchangeForm.cs
+++ TestCurrencyExchangeForm.cs
@@ -18,6 +18,26 @@ namespace FinancialManager
 
             moneyTextBox.Text = "0";
             dateTimePicker.Value = DateTime.Today;
+
+            LoadCurrencies();
+        }
+
+        private void LoadCurrencies()
+        {
+            var fromCurrencies = SqliteDataAccess.LoadCurrencies();
+            var toCurrencies = new List<CurrencyModel>(fromCurrencies);
+            var fromBindingSource = new BindingSource();
+            var toBindingSource = new BindingSource();
+            fromBindingSource.DataSource = fromCurrencies;
+            toBindingSource.DataSource = toCurrencies;
+
+            fromCurrencyComboBox.DataSource = fromBindingSource.DataSource;
+            fromCurrencyComboBox.DisplayMember = "FullName";
+            fromCurrencyComboBox.ValueMember = "Id";
+
+            toCurrencyComboBox.DataSource = toBindingSource.DataSource;
+            toCurrencyComboBox.DisplayMember = "FullName";
+            toCurrencyComboBox.ValueMember = "Id";
         }
     }
 }
