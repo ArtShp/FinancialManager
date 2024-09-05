036035ef0fe83a6f3b29c78301185312af670cb8
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-05 20:08:06 +0200

    Add setting: main currency id

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace FinancialManager.Properties {
                 this["PathToDb"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("-1")]
+        public long MainCurrencyId {
+            get {
+                return ((long)(this["MainCurrencyId"]));
+            }
+            set {
+                this["MainCurrencyId"] = value;
+            }
+        }
     }
 }
