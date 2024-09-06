8a5b80290262c12713199289d4cec76e22f5b3c0
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-06 14:49:12 +0200

    Add model for purchase analysis

---
+++ PurchaseAnalysisModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManager
+{
+    internal class PurchaseAnalysisModel
+    {
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public MoneyModel Sum { get; set; }
+        public CurrencyModel Currency { get; set; }
+        public MoneyModel Sum_By_Main_Currency { get; set; }
+        public string Place { get; set; }
+        public string TransactionType { get; set; }
+        public string Tags { get; set; }
+    }
+}
