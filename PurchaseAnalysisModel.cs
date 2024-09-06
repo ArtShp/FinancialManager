a5d3600272afdb9657dbb8115208c9e857984af8
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-06 18:04:22 +0200

    Update model
    Because of the features of SQL queries

--- PurchaseAnalysisModel.cs
+++ PurchaseAnalysisModel.cs
@@ -8,11 +8,13 @@ namespace FinancialManager
 {
     internal class PurchaseAnalysisModel
     {
+        public long Id { get; set; }
         public string Category { get; set; }
         public DateTime Date { get; set; }
         public MoneyModel Sum { get; set; }
-        public CurrencyModel Currency { get; set; }
-        public MoneyModel Sum_By_Main_Currency { get; set; }
+        public long CurrencyId { get; set; }
+        public string CurrencyText { get; set; }
+        public MoneyModel SumByMainCurrency { get; set; }
         public string Place { get; set; }
         public string TransactionType { get; set; }
         public string Tags { get; set; }
