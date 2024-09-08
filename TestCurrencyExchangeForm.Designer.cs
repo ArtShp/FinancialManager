f99d445b6ff8f899f22f82654636831a698a741b
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-09-08 12:11:43 +0200

    Add resizing

--- TestCurrencyExchangeForm.Designer.cs
+++ TestCurrencyExchangeForm.Designer.cs
@@ -38,13 +38,17 @@
             moneyTextBox = new TextBox();
             fromCurrencyComboBox = new ComboBox();
             toCurrencyComboBox = new ComboBox();
+            controlsPanel = new Panel();
+            panel1 = new Panel();
+            controlsPanel.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // titleLabel
             // 
             titleLabel.AutoSize = true;
             titleLabel.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
-            titleLabel.Location = new Point(31, 9);
+            titleLabel.Location = new Point(3, 3);
             titleLabel.Name = "titleLabel";
             titleLabel.Size = new Size(457, 65);
             titleLabel.TabIndex = 0;
@@ -53,7 +57,7 @@
             // fromCurrencyLabel
             // 
             fromCurrencyLabel.AutoSize = true;
-            fromCurrencyLabel.Location = new Point(31, 225);
+            fromCurrencyLabel.Location = new Point(3, 160);
             fromCurrencyLabel.Name = "fromCurrencyLabel";
             fromCurrencyLabel.Size = new Size(171, 32);
             fromCurrencyLabel.TabIndex = 1;
@@ -62,7 +66,7 @@
             // toCurrencyLabel
             // 
             toCurrencyLabel.AutoSize = true;
-            toCurrencyLabel.Location = new Point(371, 225);
+            toCurrencyLabel.Location = new Point(343, 160);
             toCurrencyLabel.Name = "toCurrencyLabel";
             toCurrencyLabel.Size = new Size(141, 32);
             toCurrencyLabel.TabIndex = 2;
@@ -70,7 +74,7 @@
             // 
             // convertButton
             // 
-            convertButton.Location = new Point(31, 473);
+            convertButton.Location = new Point(3, 3);
             convertButton.Name = "convertButton";
             convertButton.Size = new Size(150, 46);
             convertButton.TabIndex = 5;
@@ -81,7 +85,7 @@
             // dateLabel
             // 
             dateLabel.AutoSize = true;
-            dateLabel.Location = new Point(31, 341);
+            dateLabel.Location = new Point(3, 256);
             dateLabel.Name = "dateLabel";
             dateLabel.Size = new Size(64, 32);
             dateLabel.TabIndex = 6;
@@ -91,7 +95,7 @@
             // 
             dateTimePicker.CustomFormat = "dd.MM.yyyy";
             dateTimePicker.Format = DateTimePickerFormat.Custom;
-            dateTimePicker.Location = new Point(31, 376);
+            dateTimePicker.Location = new Point(3, 291);
             dateTimePicker.Name = "dateTimePicker";
             dateTimePicker.Size = new Size(400, 39);
             dateTimePicker.TabIndex = 7;
@@ -99,7 +103,7 @@
             // moneyLabel
             // 
             moneyLabel.AutoSize = true;
-            moneyLabel.Location = new Point(31, 109);
+            moneyLabel.Location = new Point(3, 68);
             moneyLabel.Name = "moneyLabel";
             moneyLabel.Size = new Size(89, 32);
             moneyLabel.TabIndex = 8;
@@ -107,7 +111,7 @@
             // 
             // moneyTextBox
             // 
-            moneyTextBox.Location = new Point(31, 144);
+            moneyTextBox.Location = new Point(3, 103);
             moneyTextBox.Name = "moneyTextBox";
             moneyTextBox.Size = new Size(257, 39);
             moneyTextBox.TabIndex = 9;
@@ -115,7 +119,7 @@
             // fromCurrencyComboBox
             // 
             fromCurrencyComboBox.FormattingEnabled = true;
-            fromCurrencyComboBox.Location = new Point(31, 260);
+            fromCurrencyComboBox.Location = new Point(3, 195);
             fromCurrencyComboBox.Name = "fromCurrencyComboBox";
             fromCurrencyComboBox.Size = new Size(257, 40);
             fromCurrencyComboBox.TabIndex = 10;
@@ -123,30 +127,50 @@
             // toCurrencyComboBox
             // 
             toCurrencyComboBox.FormattingEnabled = true;
-            toCurrencyComboBox.Location = new Point(371, 260);
+            toCurrencyComboBox.Location = new Point(343, 195);
             toCurrencyComboBox.Name = "toCurrencyComboBox";
             toCurrencyComboBox.Size = new Size(257, 40);
             toCurrencyComboBox.TabIndex = 11;
+            // 
+            // controlsPanel
+            // 
+            controlsPanel.Controls.Add(titleLabel);
+            controlsPanel.Controls.Add(toCurrencyComboBox);
+            controlsPanel.Controls.Add(fromCurrencyLabel);
+            controlsPanel.Controls.Add(fromCurrencyComboBox);
+            controlsPanel.Controls.Add(toCurrencyLabel);
+            controlsPanel.Controls.Add(moneyTextBox);
+            controlsPanel.Controls.Add(dateLabel);
+            controlsPanel.Controls.Add(moneyLabel);
+            controlsPanel.Controls.Add(dateTimePicker);
+            controlsPanel.Location = new Point(12, 12);
+            controlsPanel.Name = "controlsPanel";
+            controlsPanel.Size = new Size(610, 337);
+            controlsPanel.TabIndex = 12;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            panel1.Controls.Add(convertButton);
+            panel1.Location = new Point(12, 355);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(610, 55);
+            panel1.TabIndex = 13;
             // 
             // TestCurrencyExchangeForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(669, 543);
-            Controls.Add(toCurrencyComboBox);
-            Controls.Add(fromCurrencyComboBox);
-            Controls.Add(moneyTextBox);
-            Controls.Add(moneyLabel);
-            Controls.Add(dateTimePicker);
-            Controls.Add(dateLabel);
-            Controls.Add(convertButton);
-            Controls.Add(toCurrencyLabel);
-            Controls.Add(fromCurrencyLabel);
-            Controls.Add(titleLabel);
+            ClientSize = new Size(634, 419);
+            Controls.Add(panel1);
+            Controls.Add(controlsPanel);
+            MinimumSize = new Size(660, 490);
             Name = "TestCurrencyExchangeForm";
-            Text = "TestCurrencyExchangeForm";
+            Text = "Test Currency Exchange";
+            controlsPanel.ResumeLayout(false);
+            controlsPanel.PerformLayout();
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -161,5 +185,7 @@
         private TextBox moneyTextBox;
         private ComboBox fromCurrencyComboBox;
         private ComboBox toCurrencyComboBox;
+        private Panel controlsPanel;
+        private Panel panel1;
     }
 }
